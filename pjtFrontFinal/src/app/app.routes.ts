1eb780192481fc70b34592b1de2e298efa6400db
amassir <abdellahmassir2@gmail.com>
2025-02-09 20:31:38 +0100

    Delete pjtFrontFinal directory because of non-existence of app.module.ts

--- pjtFrontFinal/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ListerPersonnelComponent } from './lister-personnel/lister-personnel.component';
-import { AjouterPersonnelComponent } from './ajouter-personnel/ajouter-personnel.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: 'lister-personnel', pathMatch: 'full' },
-    { path: 'lister-personnel', component: ListerPersonnelComponent },
-    { path: 'ajouter-personnel', component: AjouterPersonnelComponent },
-];
