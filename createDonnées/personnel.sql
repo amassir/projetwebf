de7bf3bb79fadf1c8868d850a6b7575b7d8f2f2d
Sdoufa26 <youssef.sadfi2020@gmail.com>
2025-02-10 14:33:55 +0100

    Create personnel.sql

---
+++ createDonnées/personnel.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Personnel (
+    idP INT,
+    nomP VARCHAR(255),
+    prenomP VARCHAR(255),
+    dateEmbaucheP DATE,
+    activiteP VARCHAR(255),
+    statutP VARCHAR(255),
+    CONSTRAINT pk_idP PRIMARY KEY (idP)
+);
