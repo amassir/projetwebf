cccb805b2d2f644d117371199218c26ccca30f4c
Youssef Sadfi <youssef.sadfi2020@gmail.com>
2025-02-13 10:41:29 +0100

    changement interface personnel

---
+++ PjtFrontFinal/src/app/personnel-recommendation/personnel-recommendation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personnel-recommendation',
+  standalone: false,
+  templateUrl: './personnel-recommendation.component.html',
+  styleUrl: './personnel-recommendation.component.css'
+})
+export class PersonnelRecommendationComponent {
+
+}
