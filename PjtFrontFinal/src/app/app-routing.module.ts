17dc9c76f8268296a39355bfc712e372be1df4e0
Youssef Sadfi <youssef.sadfi2020@gmail.com>
2025-02-14 08:23:20 +0100

    Résolution de la route recommendation personnel

--- PjtFrontFinal/src/app/app-routing.module.ts
+++ PjtFrontFinal/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PersonnelComponent } from './personnel/personnel.component';
 import { MissionsComponent } from './missions/missions.component';
+import { PersonnelRecommendationComponent } from './personnel-recommendation/personnel-recommendation.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'personnel', component: PersonnelComponent},
   {path: 'missions', component: MissionsComponent },
+  {path: 'personnel-recommendation', component: PersonnelRecommendationComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: '**', component: HomeComponent},
 ];
